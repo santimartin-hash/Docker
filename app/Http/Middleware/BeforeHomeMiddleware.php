a367d91fca0ed27bd9cb26c100c4eff30e3287a2
Santiago <ikcfa@plaiaundi.net>
2023-11-29 09:15:59 +0100

    segundocomit

---
+++ app/Http/Middleware/BeforeHomeMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Log;
+
+
+
+class BeforeHomeMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle($request, Closure $next)
+    {
+        Log::info('Middleware ejecutado Antes de cargar la página home');
+        
+        $response = $next($request);
+        return $response;
+        
+    }
+}
